543596c089f61a6b64b885eeb01166cd23d1ad38
Emily Yoshida-Gorgonia <emily@finoch.io>
2025-07-11 15:05:43 +0000

    use a modern tech focused sans seriff font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,7 @@ export default {
     extend: {
       fontFamily: {
         body: ['Inter', 'sans-serif'],
-        headline: ['EB Garamond', 'serif'],
+        headline: ['Space Grotesk', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
