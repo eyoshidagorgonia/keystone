6fff8ad9be5b6ca2f9568b6097efdd71f3f56b5e
Emily Yoshida-Gorgonia <emily@finoch.io>
2025-07-12 00:14:48 +0000

    Error: ./src/app/layout.tsx:3:1 Module not found: Can't resolve '@/compo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,8 @@
 
 import { getApiKeys } from "@/lib/apiKeyService";
 import { DashboardClientPage } from "@/components/dashboard-client-page";
-import { redirect } from 'next/navigation'
-
 
 export default async function DashboardPage() {
-  if (process.env.KEYSTONE_MODE === 'api') {
-    redirect('/404');
-  }
   const apiKeys = await getApiKeys();
-
   return <DashboardClientPage initialKeys={apiKeys} />;
 }
