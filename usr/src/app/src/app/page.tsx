d3cf9921a5c79c772a076e7c664239916190165b
Emily Yoshida-Gorgonia <emily@finoch.io>
2025-07-12 00:18:43 +0000

    admin mode Error: ./src/app/layout.tsx:3:1 Module not found: Can't resol

--- usr/src/app/src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-
-import { getApiKeys } from "@/lib/apiKeyService";
-import { DashboardClientPage } from "@/components/dashboard-client-page";
-
-export default async function DashboardPage() {
-  const apiKeys = await getApiKeys();
-  return <DashboardClientPage initialKeys={apiKeys} />;
-}
